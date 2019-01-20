f73790db191eb057f2f664b14b4eff7e57d864cf
Cyrille Bourgois <cyrille.bourgois@gmail.com>
2019-01-20 22:32:48 +0100

    use casper theme from vuepress-theme-casper

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,2 +1,64 @@
 module.exports = {
+  title: 'Cyrille Bourgois',
+  description: 'Front Developer',
+  base: '/',
+  theme: 'casper',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
+  markdown: {
+    anchor: {
+      permalink: false,
+      permalinkBefore: false
+    }
+  },
+  themeConfig: {
+    cover: 'https://source.unsplash.com/random',
+    // logo: '/me.png',`
+    nav: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Posts',
+        link: '/posts'
+      },
+      {
+        text: 'Code',
+        link: '/category/code'
+      },
+      {
+        text: 'Page',
+        link: '/a-page.html'
+      }
+    ],
+
+    footer: [
+      {
+        text: 'Latest Posts',
+        link: '/posts'
+      },
+      {
+        text: 'LinkedIn',
+        link: 'https://facebook.com/'
+      },
+      {
+        text: 'Twitter',
+        link: 'https://twitter.com/cyrillebourgois'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/cbourgois'
+      }
+    ],
+    social: {
+      github: 'https://github.com/cbourgois',
+      twitter: 'https://twitter.com/cyrillebourgois',
+      // facebook: 'https://facebook.com',
+      // xing: 'https://xing.de',
+      // instagram: 'https://instagram.com',
+      linkedin: 'https://linkedin.com/'
+    }
+  }
 };
---
+++ src/posts/hello-world.md
@@ -0,0 +1,16 @@
+---
+title: Hello world
+image: https://picsum.photos/1920/1080/?random&date=2018-04-15
+publish: 2019-01-20
+type: post
+tags:
+  - javascript
+categories:
+  - code
+readingTime: 1 Minute
+---
+
+```js
+const firstname = 'Cyrille',
+console.log(`Hello ${fistname}`);
+```
